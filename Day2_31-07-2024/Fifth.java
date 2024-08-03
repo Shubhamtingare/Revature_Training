31f5a588fb29f40a59ca5b097aede3d13f730900
Shubham Tingare <shubhamtingare0661@gmail.com>
2024-08-03 12:39:46 +0530

    Fifth program completed

---
+++ Day2_31-07-2024/Fifth.java
@@ -0,0 +1,34 @@
+public class Fifth {
+
+    public static void pattern1() {
+        for (int i = 0; i < 5; i++) {
+            for (int j = i; j < 5; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pattern2() {
+        int n = 4;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 0; j < (2 * (n - i) - 1); j++) {
+                if (j % 2 == 0) {
+                    System.out.print("1");
+                } else {
+                    System.out.print("0");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        pattern1();
+        pattern2();
+    }
+}
