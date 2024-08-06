56c8845de1a04f2572fb4bfd02309f7d5868ba1d
Shubham Tingare <shubhamtingare0661@gmail.com>
2024-08-06 15:47:49 +0530

    Eighth code completed

--- Day3_01-08-2024/Eighth.java
+++ Day3_01-08-2024/Eighth.java
@@ -3,9 +3,12 @@ import java.util.Scanner;
 
 public class Eighth {
     public static void main(String[] args) {
-        int arr[] = new int[10];
-
         Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number of elements :");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
 
         for (int i = 0; i < arr.length; i++) {
             arr[i] = sc.nextInt();
