cbf2d92ca7664ae29959588bba07106827548599
Shubham Tingare <shubhamtingare0661@gmail.com>
2024-08-10 11:40:13 +0530

    first program completed

--- Day4_07-08-2024/First.java
+++ Day4_07-08-2024/First.java
@@ -2,11 +2,17 @@
 public class First {
     public static void main(String[] args) {
         String word = "Umbrella";
+        int count = 0;
 
         for (int i = 0; i < word.length(); i++) {
-            if(word.charAt('e')){
-                System.out.println("present");
+            if (word.charAt(i) == 'e') {
+                count++;
             }
         }
+        if (count > 0) {
+            System.out.println("present");
+
+        } else
+            System.out.println("Not present");
     }
 }
