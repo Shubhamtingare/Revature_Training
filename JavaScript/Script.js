b8f6557f957c4ddcad7d707c6821b652ddb2e3ef
Shubham Tingare <shubhamtingare0661@gmail.com>
2024-08-22 10:31:52 +0530

    javascript function practice

---
+++ JavaScript/Script.js
@@ -0,0 +1,11 @@
+// "use strict";
+var x;
+x = 10;
+console.log(x);
+
+function sayHello() {
+  console.log("Hello its me !");
+}
+sayHello();
+
+
---
+++ JavaScript/functionDemo.js
@@ -0,0 +1,6 @@
+function multiply(n1, n2) {
+  return n1 * n2;
+}
+
+var result = multiply(10, 20);
+console.log("Result", result);
